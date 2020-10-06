a4937b86e75012cf0fb48b948db4a59db1fe2926
Роман Новиков <Demoi@rnovikov-pc.bsw.iron>
2020-10-06 13:20:45 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var seq = [];
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (str.charAt(i) === bracketsConfig[j][0]) {
+                if (str.charAt(i) === bracketsConfig[j][1]) {
+                    bracketsConfig[j][0] = bracketsConfig[j][0] + "o";
+                    seq.push(str.charAt(i) + "o");
+                } else {
+                    seq.push(str.charAt(i));
+                }
+            } else {
+                if (str.charAt(i) === bracketsConfig[j][1]) {
+                    if (seq[seq.length - 1] === bracketsConfig[j][0]) {
+                        if (bracketsConfig[j][0].length > 1) {
+                            bracketsConfig[j][0] = bracketsConfig[j][1];
+                        }
+                        seq.splice(-1, 1);
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+    if(seq.length>0){
+      return false;
+    }
+    return true;
 }
